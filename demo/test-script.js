4da6cd290fa842ad19f0acdf78a7bf5ac611a837
Kevin Shu <Kevin@nk218-187-15-172.dialup.dynamic.apol.com.tw>
2013-05-26 09:38:54 +0800

    v0.0.7

--- demo/test-script.js
+++ demo/test-script.js
@@ -6,6 +6,10 @@ alert("Generate todo2...");
 var todo2 = new TP("event-template",{"event":"TEST"});
 document.getElementById("todo-container").appendChild(todo2.view);
 
+alert("Generate issue ticket...");
+var ticket = new TP("issue-ticket",{"issue":"bug"});
+document.getElementById("todo-container").appendChild(ticket.view);
+
 var setTodo1 = confirm("Set Todo1's event as \"123\"?");
 if (setTodo1){
 	todo1.set("event","123");
@@ -19,4 +23,9 @@ if (setErrorData){
 var setErrorData = confirm("Set Todo1's \"check\" as \"function(viewModel){viewModel.kill();}\"?");
 if (setErrorData){
 	todo1.set( "check", function(vm){vm.kill();} );
+}
+
+var setErrorData = confirm("When change one event, alert all events?");
+if (setErrorData){
+	TP.chain("event-template",function(){alert(this.data.event)});
 }
--- totem-pole.js
+++ totem-pole.js
@@ -1,5 +1,5 @@
 /*
- * "Totem-Pole.js" v0.0.6
+ * "Totem-Pole.js" v0.0.7
  * 
  * Copyright 2013 Kevin Shu
  * Released under the MIT license
@@ -7,103 +7,170 @@
 
 (function() {
 
-    if ( 'undefined' === typeof TP ) {
+    // if ( 'undefined' === typeof TP ) {
 
-        var subscriber = { any:[] };
+    var subscriber = { any:[] },
+        groups = [],
+        // All the events that available
+        EVENTS = [  "click", "change", "focus", "keydown", "keypress", "keyup", "mousedown", "mouseout", "mouseover", "mouseup", "submit"  ];
 
-        TP = function(id, initData) {
-            this.view = document.getElementById(id).cloneNode(true);
-            this.view.id='';
-            this.data = {};
-            this.set = returnSet( this.view );
-            this.kill = kill;
-            this.html = this.view.outerHTML;
+    TP = function(id, initData) {
+        this.view = document.getElementById(id).cloneNode(true);
+        this.view.id='';
+        this.data = {};
+        this.set = returnSet( this.view );
+        this.kill = kill;
+        this.html = this.view.outerHTML;
+        this._id = 0;
+        this._group = id;
 
-            if ( (typeof initData)==="object" ) {
-                this.set(initData);
-            }
-        };
+        if ( (typeof initData)==="object" ) {
+            this.set(initData);
+        }
 
-        TP.subscribe = function (event, callback) {
-            if (typeof event == "function") {
-                callback = event;
-                event = "any";
-            }
-            subscriber[event] = subscriber[event]||[];
-            subscriber[event].push(callback);
-        };
+        if (!groups[id]){
+            groups[id] = [];
+        }
+        groups[id].push(this);
+        this._id = groups[id].length;
+    };
 
-        TP.publish = function (event, args) {
-            var i=0,
-                max=subscriber[event].length;
-            if ( (typeof event)=="object" && args==null) {
-                args = event;
-                event = "any";
-            }
-            for (;i<max;i++) {
-                subscriber[event][i].apply(null,args);
+    TP.subscribe = function (event, callback) {
+        if (typeof event == "function") {
+            callback = event;
+            event = "any";
+        }
+        subscriber[event] = subscriber[event]||[];
+        subscriber[event].push(callback);
+    };
+
+    TP.publish = function (event, args) {
+        var i=0,
+            max=subscriber[event]?subscriber[event].length:0;
+        if ( (typeof event)=="object" && args==null) {
+            args = event;
+            event = "any";
+        }
+        for (;i<max;i++) {
+            subscriber[event][i].apply(null,args);
+        }
+    };
+
+    TP.all = function(){
+        var totemPole = [],
+            key = "",
+            i = 0,
+            max = 0;
+        for (key in groups) {
+            max = groups[key].length;
+            for(i=0;i<max;i++){
+                if (groups[key][i]) {
+                    totemPole.push(groups[key][i]);
+                }
             }
         }
-    }
+        return totemPole;
+    };
 
-    // All the events that available
-    var events = [  "click",
-                    "change",
-                    "focus",
-                    "keydown",
-                    "keypress",
-                    "keyup",
-                    "mousedown",
-                    "mouseout",
-                    "mouseover",
-                    "mouseup",
-                    "submit"  ];
+    TP.group = function(groupName){
+        var totemPole = [],
+            group = groups[groupName],
+            i = 0,
+            max = group.length;
+        for(;i<max;i++){
+            if (group[i]) {
+                totemPole.push(group[i]);
+            }
+        }
+        return totemPole;
+    };
+
+    TP.chain = function(group, callback){
+        if(group == "all"){
+
+        } else {
+            (function(group, callback){
+                TP.subscribe( group, function(){
+                    var totemPole = TP.group(group),
+                        i = 0,
+                        max = totemPole.length;
+                    for (; i<max; i++) {
+                        callback.call(totemPole[i]);
+                    }
+                });
+            })(group, callback);
+        }
+    };
+
+    TP.clearGroup = function(){
+        for (var key in groups) {
+            delete groups[key];
+        }
+    };
+
+    initialize();
+    
+    // }
 
     function returnSet(view) {
 
-        var elemData = parseDom.call(view,{});
+        var elemDatas = parseDom.call(view,{});
 
+        // Set one attritute a time
         function setByOnce(key, value) {
-            if ( elemData.hasOwnProperty(key) ) { // If illegal data pass in, ignore it.
-                var _this = elemData[key];
-                renderView.call(_this, value, this); // "this" presents viewModel
+            if ( elemDatas.hasOwnProperty(key) ) { // If illegal data pass in, ignore it.
+                var _this = elemDatas[key];
                 this.data[key] = value;
             }
         }
 
         return  function (key, value) {
+                    var settings = {};
                     if ( (typeof key)==="object" ) {
-                        var data = key;
-                        for ( var _key in data) {
-                            setByOnce.call(this, _key, data[_key]);
+                        settings = key;
+                        for ( var _key in settings) {
+                            setByOnce.call(this, _key, settings[_key]);
                         }
                     } else if ( (typeof key)==="string" && ["string","object","function"].indexOf(typeof value)!==-1 ) {
                         setByOnce.call(this, key, value);
+                        settings[key] = value;
                     }
-                    this.html = this.view.outerHTML;
+                    TP.publish("data-change", [this, elemDatas, settings]);
                 }
     }
 
     // This function will be called by each of viewModel's data.
-    function renderView(data, viewModel) {
-        var elem = this.element;
-        var type = this.type;
-        if ( type==="style" ) {
-            for ( var _key in data ) {
-                elem.style[_key]=data[_key];
+    function renderView(viewModel, elemDatas, settings) {
+        for (var key in settings) {
+
+            // If illegal data pass in, ignore it.
+            if (!elemDatas.hasOwnProperty(key)) {continue;}
+
+            var elem = elemDatas[key].element,
+                type = elemDatas[key].type,
+                data = settings[key];
+            if ( type==="style" ) {
+                for ( var _key in data ) {
+                    elem.style[_key]=data[_key];
+                }
+            } else if (type==="attr") {
+                for ( var _key in data){
+                    elem[_key]=data[_key];
+                }
+            } else if ( type==="html" ) {
+                elem.innerHTML = data;
+            } else if ( EVENTS.indexOf(type)!==-1 ) {
+                (function(elem, data){
+                    elem.addEventListener(type, function(e){ data.call(elem, viewModel); });
+                })(elem, data);
+            } else if ( type==="text" || type==="" ) {
+                elem.innerText = data;
+            } else if ( type==="value" ) {
+                elem.value = data;
+                elem.addEventListener( "change", function(e){ TP.publish("ui-change", [viewModel, key, elem.value]); } );
+            } else {
+                elem[type]=data;
             }
-        } else if (type==="attr") {
-            for ( var _key in data){
-                elem[_key]=data[_key];
-            }
-        } else if ( type==="html" ) {
-            elem.innerHTML = data;
-        } else if ( events.indexOf(type)!==-1 ) {
-            elem["on"+type] = function(e){ data.call(elem,viewModel);};
-        } else if ( type==="text" || type==="" ) {
-            elem.innerText = data;
-        } else {
-            elem[type]=data;
         }
     }
 
@@ -128,6 +195,19 @@
 
     function kill() {
         this.view.remove();
+        delete groups[this._group][this._id];
+    }
+
+    function initialize() {
+        TP.subscribe("ui-change", function(viewModel, key, value){
+            viewModel.data[key] = value;
+            TP.publish(viewModel._group);
+        });
+        TP.subscribe("data-change", function(viewModel, elemDatas, settings){
+            renderView(viewModel, elemDatas, settings); // "this" represents viewModel
+            viewModel.html = viewModel.view.outerHTML;
+            TP.publish(viewModel._group);
+        });
     }
     
 })();
