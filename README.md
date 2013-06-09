60ec2461fc4c02122befc1181e973ff94567e978
Kevin Shu <Kevin@kh218-187-31-53.dialup.dynamic.apol.com.tw>
2013-06-09 23:57:58 +0800

    v0.0.8

--- README.md
+++ README.md
@@ -5,19 +5,36 @@ A lightweight, handy, No dependencies MVVM framework of javascript.
 
 Watch the tutorial page [here](http://kevin-shu.github.com/totem-pole/).
 
+How to use it?
+--------------
+
+
+
 Data-binding type:
 ------------------
-* html
-* text
-* event
-* attr
-* others
+### html, text
+Binding DOM object's innerHTML or innerText with view-model's data.
+
+### event
+Attach an event-handler to DOM object's attribute.
+
+### attr
+Multi-binding between view-model's data and view with an past-in object.
+
+### value
+When you bind an view-model's data with "input", "select", "textarea". It would creat an two-way binding: UI to data, data to UI. It's automatic! 
+
+### style
+
 
 ViewModel's attributes & methods:
 ---------------------------------
-* view - dom object
-* html - html string
+* view - View's DOM object
+* html - View's innerHTML.
 * data - viewModel's data
-* kill() - remove the viewModel's view
+* kill() - Remove the viewModel, also remove the view.
 * set(data) - pass a data object to batch-update viewModel's data
 * set(key, value) - set a data "key" as "value"
+
+
+
--- totem-pole.js
+++ totem-pole.js
@@ -1,5 +1,5 @@
 /*
- * "Totem-Pole.js" v0.0.7
+ * "Totem-Pole.js" v0.0.8
  * 
  * Copyright 2013 Kevin Shu
  * Released under the MIT license
@@ -15,24 +15,34 @@
         EVENTS = [  "click", "change", "focus", "keydown", "keypress", "keyup", "mousedown", "mouseout", "mouseover", "mouseup", "submit"  ];
 
     TP = function(id, initData) {
-        this.view = document.getElementById(id).cloneNode(true);
-        this.view.id='';
-        this.data = {};
-        this.set = returnSet( this.view );
-        this.kill = kill;
-        this.html = this.view.outerHTML;
-        this._id = 0;
-        this._group = id;
+        var view = (id=="body") ? document.body : document.getElementById(id);
 
-        if ( (typeof initData)==="object" ) {
-            this.set(initData);
-        }
+        if (initData instanceof Array) {
+            var that = [];
+                i=1,
+                max=initData.length;
+            that = [];
+            that.push( Totem.call({view:view},initData[0],id) );
 
-        if (!groups[id]){
-            groups[id] = [];
+            for(;i<max;i++){
+                var clonedView = view.cloneNode(true),
+                    totem = Totem.call({view:clonedView},initData[i],id);
+                clonedView.id = '';
+                that.push(totem);
+                insertAfter(clonedView, view); // Insert after the last node.
+                view = clonedView; // Set view as last node.
+            }
+            return that;
+        } else {
+            return Totem.call({view:view},initData);
         }
-        groups[id].push(this);
-        this._id = groups[id].length;
+    };
+
+    TP.template = function(id, initData){
+        var that={};
+        that.view = document.getElementById(id).cloneNode(true);
+        Totem.call(that, initData, id);
+        return that;
     };
 
     TP.subscribe = function (event, callback) {
@@ -102,25 +112,67 @@
         }
     };
 
-    TP.clearGroup = function(){
-        for (var key in groups) {
-            delete groups[key];
-        }
-    };
-
     initialize();
     
     // }
 
-    function returnSet(view) {
+    function Totem(initData, groupName) {
+        this.view.id='';
+        this.data = {};
+        this.set = returnSet( this );
+        this.kill = kill;
+        this.html = this.view.outerHTML;
+        this._id = 0;
+        this._group = groupName;
 
-        var elemDatas = parseDom.call(view,{});
+        if ( (typeof initData)==="object" ) {
+            this.set(initData);
+        }
+
+        if (!groups[groupName]){
+            groups[groupName] = [];
+        }
+        this._id = groups[groupName].length;
+        groups[groupName].push(this);
+    }
+
+
+    function returnSet(viewModel) {
+
+        var elemDatas = parseDom.call(viewModel.view,{}),
+            key = "",
+            data = {};
+
+        // This section is for fetching the data already existed in the template.
+        // And after that, Totem-pole will "renderView" with the data just fetched from the template.
+        // In this way, the "ui-binding" would be established.
+        // And the data in the template are just like the default value.
+        for (key in elemDatas) {
+            var i = 0,
+                max = elemDatas[key].length;
+            data[key] = null;
+            for (;i<max;i++) {
+                var element = elemDatas[key][i].dom,
+                    type = elemDatas[key][i].type;
+                if ( type==="html" && element.innerHTML ) {
+                    data[key] = element.innerHTML;
+                } else if ( type==="text" && element.innerText ) {
+                    data[key] = element.innerText;
+                } else if ( type==="value" && element.value ) {
+                    data[key] = element.value;
+                }
+            }
+        }
+        renderView(viewModel, elemDatas, data);
+        // END....2013.6.9 v0.0.8
 
         // Set one attritute a time
         function setByOnce(key, value) {
             if ( elemDatas.hasOwnProperty(key) ) { // If illegal data pass in, ignore it.
                 var _this = elemDatas[key];
-                this.data[key] = value;
+                if (typeof value != "function") {
+                    this.data[key] = value;
+                }
             }
         }
 
@@ -146,30 +198,38 @@
             // If illegal data pass in, ignore it.
             if (!elemDatas.hasOwnProperty(key)) {continue;}
 
-            var elem = elemDatas[key].element,
-                type = elemDatas[key].type,
-                data = settings[key];
-            if ( type==="style" ) {
-                for ( var _key in data ) {
-                    elem.style[_key]=data[_key];
+            var elements = elemDatas[key],
+                type = "",
+                element = {},
+                data = settings[key],
+                i=0,
+                max = elements.length;
+            for (;i<max;i++){
+                type=elements[i].type;
+                element=elements[i].dom;
+                if ( type==="style" ) {
+                    for ( var _key in data ) {
+                        element.style[_key]=data[_key];
+                    }
+                } else if (type==="attr") {
+                    for ( var _key in data){
+                        element[_key]=data[_key];
+                    }
+                } else if ( type==="html" ) {
+                    element.innerHTML = data;
+                } else if ( EVENTS.indexOf(type)!==-1 ) {
+                    (function(elem, data){
+                        elem.addEventListener(type, function(e){ data.call(elem, viewModel); });
+                    })(element, data);
+                } else if ( type==="text" ) {
+                    element.innerText = data;
+                } else if ( type==="value" ) {
+                    element.value = data;
+                    // element.addEventListener( "change", function(e){ viewModel.set(key, element.value);/*TP.publish("ui-change", [viewModel, key, element.value]);*/ } );
+                    element.onchange = function(e){ viewModel.set(key, element.value);/*TP.publish("ui-change", [viewModel, key, element.value]);*/ } ;
+                } else {
+                    element[type]=data;
                 }
-            } else if (type==="attr") {
-                for ( var _key in data){
-                    elem[_key]=data[_key];
-                }
-            } else if ( type==="html" ) {
-                elem.innerHTML = data;
-            } else if ( EVENTS.indexOf(type)!==-1 ) {
-                (function(elem, data){
-                    elem.addEventListener(type, function(e){ data.call(elem, viewModel); });
-                })(elem, data);
-            } else if ( type==="text" || type==="" ) {
-                elem.innerText = data;
-            } else if ( type==="value" ) {
-                elem.value = data;
-                elem.addEventListener( "change", function(e){ TP.publish("ui-change", [viewModel, key, elem.value]); } );
-            } else {
-                elem[type]=data;
             }
         }
     }
@@ -184,10 +244,17 @@
         if (this.dataset && this.dataset['tp']) {
             var attrs = this.dataset['tp'].replace(/\s/g, '').split(",");
             for ( var _i in attrs) {
-                tpData[ attrs[_i].split(":")[0] ] = {
-                    type: attrs[_i].split(":")[1] || "",
-                    element: this
-                };
+                if (tpData[ attrs[_i].split(":")[0] ]) { // If this data has been registered
+                    tpData[ attrs[_i].split(":")[0] ].push({
+                        type: attrs[_i].split(":")[1] || "text",
+                        dom: this
+                    });
+                } else {
+                    tpData[ attrs[_i].split(":")[0] ] = [{
+                        type: attrs[_i].split(":")[1] || "text",
+                        dom: this
+                    }];
+                }
             }
         }
         return tpData;
@@ -199,15 +266,29 @@
     }
 
     function initialize() {
-        TP.subscribe("ui-change", function(viewModel, key, value){
-            viewModel.data[key] = value;
-            TP.publish(viewModel._group);
-        });
+        // TP.subscribe("ui-change", function(viewModel, key, value){
+        //     viewModel.data[key] = value;
+        //     TP.publish(viewModel._group);
+        //     TP.publish("all");
+        // });
         TP.subscribe("data-change", function(viewModel, elemDatas, settings){
             renderView(viewModel, elemDatas, settings); // "this" represents viewModel
             viewModel.html = viewModel.view.outerHTML;
             TP.publish(viewModel._group);
+            TP.publish("all");
         });
+    }
+
+    function insertAfter(newElement, ref) {
+        var refParent = ref.parentNode;
+        var refNext = ref.nextSibling;
+
+        if (refNext === null) {
+            refParent.appendChild(newElement);
+        } else {
+            refParent.insertBefore(newElement, refNext);
+        }
+        return newElement;
     }
     
 })();
